41064fbc9102a910f7025c0988f51d773569bb5c
BrVecchi <bno.vecchi@gmail.com>
2022-11-23 19:04:11 -0300

    feat: cartController with postCart route

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,13 @@
 import express from "express"
 import { getProducts } from "../controllers/productsController.js"
+import { signIn, signUp } from "../controllers/authController.js"
+import { postCart } from "../controllers/cartController.js"
 
 export const router = express.Router()
 
 router.get("/products", getProducts)
 router.post("/sign-in", signIn)
 router.post("/sign-up", signUp)
+router.post("/cart", postCart)
+router.get("/cart", getCart)
+router.delete("/cart", deleteCart)
