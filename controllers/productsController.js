7ec17d804a998abd427fda18c13c5e7a8e16c054
BrVecchi <bno.vecchi@gmail.com>
2022-11-24 11:10:09 -0300

    fix: get products

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -5,8 +5,8 @@ const productsCollection = db.collection("products")
 export const getProducts = async (req, res) => {
     const user = req.body
     try {
-        await productsCollection.find().toArray()
-        res.sendStatus(201)
+        const products = await productsCollection.find().toArray()
+        res.status(201).send(products)
     } catch (error) {
         res.status(404).send(error)
     }
