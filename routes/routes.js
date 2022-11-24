b4a1dfe7feaa12f23aede4356e22806b9f65123e
BrVecchi <bno.vecchi@gmail.com>
2022-11-24 10:48:32 -0300

    fix: routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,7 @@
 import express from "express"
 import { getProducts } from "../controllers/productsController.js"
 import { signIn, signUp } from "../controllers/authController.js"
-import { postCart } from "../controllers/cartController.js"
+import { getCart, postCart } from "../controllers/cartController.js"
 import { tokenMiddleware } from "../middlewares/tokenValidationMiddleware.js"
 
 export const router = express.Router()
@@ -11,4 +11,4 @@ router.post("/sign-in", signIn)
 router.post("/sign-up", signUp)
 router.post("/cart",tokenMiddleware, postCart)
 router.get("/cart",tokenMiddleware, getCart)
-router.delete("/cart",tokenMiddleware, deleteCart)
+// router.delete("/cart",tokenMiddleware, deleteCart)
