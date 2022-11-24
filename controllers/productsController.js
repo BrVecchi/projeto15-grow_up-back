363876c5c7158a6cd6248b15ab6aafe60907a91a
BrVecchi <bno.vecchi@gmail.com>
2022-11-24 11:21:02 -0300

    fix get products

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -3,10 +3,10 @@ import db from "../src/db.js"
 const productsCollection = db.collection("products")
 
 export const getProducts = async (req, res) => {
-    const user = req.body
     try {
         const products = await productsCollection.find().toArray()
         res.status(201).send(products)
+        console.log(products)
     } catch (error) {
         res.status(404).send(error)
     }
